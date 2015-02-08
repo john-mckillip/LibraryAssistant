f0f340713b64c70156380c911992b0bfbb59958b
john-mckillip <john0mckillip@gmail.com>
2015-02-08 11:46:03 -0600

    Initial commit.

---
+++ LibraryModels/Book.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibraryModels
+{
+    class Book
+    {
+        private int id { get; set; }
+        private string title { get; set;}
+        private string author { get; set; }
+        private string publisher { get; set; }
+        private double isbnNum { get; set; }
+        private bool isCheckedOut { get; set; }
+
+        public Book(string t, string a, string p, double i)
+        {
+            title = t;
+            author = a;
+            publisher = p;
+            isbnNum = i;
+            isCheckedOut = false;
+
+            // ToDo: Build a way to store master id so it is
+            // always unique
+        }
+    }
+}
