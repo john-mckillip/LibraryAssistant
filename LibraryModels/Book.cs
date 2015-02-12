286389e9ed5dd3ae2bdddc6be82e6b91bf266d70
john0mckillip@gmail.com <john0mckillip@gmail.com>
2015-02-12 17:05:25 -0600

    Started work on methods to serialize Book object.

--- LibraryModels/Book.cs
+++ LibraryModels/Book.cs
@@ -2,12 +2,16 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.IO;
 using System.Threading.Tasks;
+using System.Runtime.Serialization;
+using System.Runtime.Serialization.Formatters.Binary;
+
 
 namespace LibraryModels
 {
-    [Serializable]
-    public class Book
+    [Serializable()]
+    public class Book : ISerializable
     {
         private int id { get; set; }
         private string title { get; set;}
@@ -19,14 +23,32 @@ namespace LibraryModels
         // Contstructor
         public Book(string t, string a, string p, string iNum)
         {
-            title = t;
-            author = a;
-            publisher = p;
-            isbnNum = iNum;
-            isCheckedOut = false;
+            this.title = t;
+            this.author = a;
+            this.publisher = p;
+            this.isbnNum = iNum;
+            this.isCheckedOut = false;
 
             // ToDo: Build a way to create/store master id so it is
             // always unique
+        }
+
+        // Deserialization method
+        public Book(SerializationInfo info, StreamingContext ctxt)
+        {
+
+        }
+
+        // Serialization method http://tech.pro/tutorial/618/csharp-tutorial-serialize-objects-to-a-file
+        public void GetObjectData(SerializationInfo info, StreamingContext ctxt)
+        {
+            info.AddValue("BookId", this.id);
+            info.AddValue("BookTitle", this.title);
+            info.AddValue("BookAuthor", this.author);
+            info.AddValue("BookPublisher", this.publisher);
+            info.AddValue("ISBNNumber", this.isbnNum);
+            info.AddValue("CheckedOut", this.isCheckedOut);
+
         }
     }
 }
